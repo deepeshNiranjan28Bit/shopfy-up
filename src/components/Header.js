ccbf5246abbe6895fe5d127a50521dc3f3c36497
webdesigner-28bit <deepeshniranjan@gmail.com>
2022-01-15 15:12:14 +0530

    done responsive nature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,80 @@
 import React from "react";
 import "../css/header.css";
+import { useMediaQuery } from "../misc/custom-hooks";
 import Logo from "../images/logo.svg";
 import CartImg from "../images/icon-cart.svg";
 import AvatarImg from "../images/image-avatar.png";
+import MenuImg from "../images/icon-menu.svg";
 
 function Header() {
+  const isMobile = useMediaQuery("( max-width : 700px )");
   return (
-    <div className="header-nav-div">
-      <div className="nav-logo">
-        <img src={Logo} alt="logo" />
-      </div>
-      <div className="nav-left">
-        <a href="#collections" className="nav-link nav-link-grow-up">
-          Collections
-        </a>
-        <a href="#men" className="nav-link nav-link-grow-up">
-          Men
-        </a>
-        <a href="#women" className="nav-link nav-link-grow-up">
-          Women
-        </a>
-        <a href="#about" className="nav-link nav-link-grow-up">
-          About
-        </a>
-        <a href="#contact" className="nav-link nav-link-grow-up">
-          Contact
-        </a>
-      </div>
-      <div className="nav-right">
-        <div style={{ marginRight: "20px" }}>
-          <img
-            src={AvatarImg}
-            alt="avatar"
-            height="40px"
-            width="40px"
-            className="avatar-pic"
-          />
-        </div>
-        <div style={{ marginRight: "20px" }} className="cart-div">
-          <img src={CartImg} alt="cart" />
-        </div>
-      </div>
+    <div className={isMobile ? "header-nav-div-res" : "header-nav-desk"}>
+      {isMobile ? (
+        <>
+          <div className="menu-res-logo">
+            <img src={MenuImg} alt="menu" style={{ marginLeft: "20px" }} />
+
+            <img src={Logo} alt="logo" style={{ marginLeft: "20px" }} />
+          </div>
+
+          <div className="menu-res-cart">
+            <img
+              src={CartImg}
+              alt="cart"
+              style={{ marginRight: "20px" }}
+              className="cart-div"
+            />
+
+            <img
+              src={AvatarImg}
+              alt="avatar"
+              height="40px"
+              width="40px"
+              className="avatar-pic"
+              style={{ marginRight: "20px" }}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="nav-logo">
+            <img src={Logo} alt="logo" />
+          </div>
+          <div className="nav-left">
+            <a href="#collections" className="nav-link nav-link-grow-up">
+              Collections
+            </a>
+            <a href="#men" className="nav-link nav-link-grow-up">
+              Men
+            </a>
+            <a href="#women" className="nav-link nav-link-grow-up">
+              Women
+            </a>
+            <a href="#about" className="nav-link nav-link-grow-up">
+              About
+            </a>
+            <a href="#contact" className="nav-link nav-link-grow-up">
+              Contact
+            </a>
+          </div>
+
+          <div className="nav-right">
+            <div style={{ marginRight: "20px" }}>
+              <img
+                src={AvatarImg}
+                alt="avatar"
+                height="40px"
+                width="40px"
+                className="avatar-pic"
+              />
+            </div>
+            <div style={{ marginRight: "20px" }} className="cart-div">
+              <img src={CartImg} alt="cart" />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlexboxGrid, Col } from "rsuite";
 import "../css/items.css";
+import { useMediaQuery } from "../misc/custom-hooks";
 import CartImg from "../images/icon-cart2.svg";
 import Minus from "../images/icon-minus.svg";
 import Plus from "../images/icon-plus.svg";
@@ -14,56 +15,86 @@ import Image3 from "../images/image-product-3.jpg";
 import Image4 from "../images/image-product-4.jpg";
 
 function Items() {
+  const isMobile = useMediaQuery("( max-width : 700px )");
   return (
-    <div className="items-div">
+    <div className={isMobile ? "items-div-res" : "items-div"}>
       <FlexboxGrid align="middle" justify="space-around">
-        <FlexboxGrid.Item as={Col} colspan={24} md={12} lg={9}>
+        <FlexboxGrid.Item
+          as={Col}
+          colspan={24}
+          sm={11}
+          lg={9}
+          style={{ paddingLeft: "0" }}
+        >
           <div style={{ textAlign: "center", maxWidth: "max-content" }}>
-            <img src={Image1} alt="" className="modal-img" />
-            <FlexboxGrid justify="space-between" style={{ marginTop: "5%" }}>
-              <FlexboxGrid.Item colspan={4}>
-                <img src={Image1Thumb} alt="" className="thumbnailimg" />
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item colspan={4}>
-                <img src={Image2Thumb} alt="" className="thumbnailimg" />
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item colspan={4}>
-                <img src={Image3Thumb} alt="" className="thumbnailimg" />
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item colspan={4}>
-                <img src={Image4Thumb} alt="" className="thumbnailimg" />
-              </FlexboxGrid.Item>
-            </FlexboxGrid>
+            <img
+              src={Image1}
+              alt=""
+              className={isMobile ? "modal-img-res" : "modal-img"}
+            />
+            {!isMobile && (
+              <FlexboxGrid justify="space-between" style={{ marginTop: "5%" }}>
+                <FlexboxGrid.Item colspan={4}>
+                  <img src={Image1Thumb} alt="" className="thumbnailimg" />
+                </FlexboxGrid.Item>
+                <FlexboxGrid.Item colspan={4}>
+                  <img src={Image2Thumb} alt="" className="thumbnailimg" />
+                </FlexboxGrid.Item>
+                <FlexboxGrid.Item colspan={4}>
+                  <img src={Image3Thumb} alt="" className="thumbnailimg" />
+                </FlexboxGrid.Item>
+                <FlexboxGrid.Item colspan={4}>
+                  <img src={Image4Thumb} alt="" className="thumbnailimg" />
+                </FlexboxGrid.Item>
+              </FlexboxGrid>
+            )}
           </div>
         </FlexboxGrid.Item>
-        <FlexboxGrid.Item as={Col} colspan={24} md={12} lg={10}>
-          <div>
+        <FlexboxGrid.Item as={Col} colspan={24} sm={11} lg={10}>
+          <div className={isMobile && "content-div-res"}>
             <strong style={{ color: "#f58840" }}>SNEAKER COMPANY</strong>
-            <h2>Fall Limited Edition Sneakers</h2>
+            <h2 style={{ color: "#000" }}>Fall Limited Edition Sneakers</h2>
             <br />
-            These low profile sneakers are your perfect casual wear
-            companion.Featuring a durable rubber outer sole, they'll withstand
-            everything the weather can offer.
+            <h6 style={{ fontWeight: !isMobile && "300" }}>
+              These low profile sneakers are your perfect casual wear
+              companion.Featuring a durable rubber outer sole, they'll withstand
+              everything the weather can offer.
+            </h6>
             <br />
             <br />
-            <h4 style={{ display: "inline" }}>
+            <h3 style={{ display: "inline", color: "#000" }}>
               $125 <span className="discount-div">50%</span>
-            </h4>
-            <p style={{ textDecoration: "line-through", color: "#D3D3D3" }}>
+            </h3>
+            <h6
+              style={{ textDecoration: "line-through", color: "#D3D3D3" }}
+              className={isMobile && "line-through-price-res"}
+            >
               $250
-            </p>
+            </h6>
             <br />
-            <button className="counter-btn">
+            <button
+              className={
+                isMobile ? "res-button counter-btn-res" : "counter-btn"
+              }
+            >
               <span>
                 <img src={Minus} alt="Minus" />
               </span>
-              &nbsp; &nbsp; &nbsp; 0 &nbsp; &nbsp; &nbsp;
+              {isMobile ? (
+                <span>0</span>
+              ) : (
+                <span>&nbsp;&nbsp;&nbsp;&nbsp; 0 &nbsp;&nbsp;&nbsp;&nbsp;</span>
+              )}
               <span>
                 <img src={Plus} alt="plus" />
               </span>
             </button>
             &nbsp;&nbsp;&nbsp;
-            <button className="addtocart-btn">
+            <button
+              className={
+                isMobile ? "res-button addtocart-btn" : "addtocart-btn"
+              }
+            >
               <img src={CartImg} alt="cart" /> &nbsp; Add To Cart
             </button>
           </div>
